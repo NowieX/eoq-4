a2c3132e369893bfef5d99f5bed42ce21d971962
Erwin de Vries <esdevries313@gmail.com>
2024-04-25 10:15:24 +0200

    add poetry

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import time
 import serial
 import serial.tools.list_ports
 
-from WaterRowerConnection import WaterRowerConnection
+from Python.WaterRowerConnection import WaterRowerConnection
 
 
 class Example:
