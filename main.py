27c85e9d04de018730dc4aa8b780213e578984b6
Noah <554456@student.saxion.nl>
2024-04-26 13:27:53 +0200

    Changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 import serial
 import serial.tools.list_ports
 import random
-from simple_board_printer import board_printer
+from simple_board_printer import board_printer, print_crowd
 from Python.WaterRowerConnection import WaterRowerConnection
 
 
@@ -17,21 +17,32 @@ class Example:
     def onDisconnect(self):
         self.connection = None
 
+    def start_countdown(self):
+        print("Get ready...")
+        for i in range(3, 0, -1):
+            print(i)
+            time.sleep(1)
+        print("Go!!!")
+
     def run(self):
         # Start monitoring in the background, calling onEvent when data comes in.
         self.connect()
 
         # This is where you run your main application. For instance, you could start a Flask app here,
         # run a GUI, do a full-screen blessed virtualization, or just about anything else.
+
+        # self.start_countdown()
         while self.connection:
+
             self.connection.requestStatistic("watts")
             if self.pulses > 0:
                 print(self.all_events)
                 speed_value = self.all_events[-1].get('value')
-                speed_value = 0 if speed_value is None else speed_value // 100
+                speed_value = 0 if speed_value is None else speed_value // 5
 
-                self.player_run_speed += random.randint(0, 5)
+                self.player_run_speed += speed_value
             board_printer(self.player_run_speed)
+            print_crowd()
 
             # calc_calories(self.pulses)
             time.sleep(1)
--- simple_board_printer.py
+++ simple_board_printer.py
@@ -1,13 +1,12 @@
 import blessed
 import random
 position = 0
-BOARD_END = 30 + 4
+BOARD_END = 200
 
 
 def board_printer(walk_speed: int):
     terminal = blessed.Terminal()
     print(terminal.home + terminal.normal + terminal.clear)
-    print(f"This is the walk speed: {walk_speed}")
 
     player_extra_speed = random.randint(1, 2)
     player = f"""
@@ -51,9 +50,27 @@ def board_printer(walk_speed: int):
 
     if walk_speed + player_extra_speed >= BOARD_END:
         print("You have won!")
+        exit()
     elif walk_speed + bob_random >= BOARD_END:
         print("Bob has won!")
+        exit()
     elif walk_speed + jan_random >= BOARD_END:
         print("Jan has won!")
+        exit()
     elif walk_speed + kim_random >= BOARD_END:
         print("Kim has won!")
+        exit()
+
+def print_crowd():
+    crowd_reactions = ["Gogogo!", "You can do it!", "Keep going!", "Don't give up!", "End of quarter sucks!!!"]
+    crowd = f"""
+{'      (˵ ͡° ͜ʖ ͡°˵)      (̿▀̿ ̿Ĺ̯̿̿▀̿ ̿)̄    ヽ༼ຈل͜ຈ༽ﾉ       ' * 3}
+"""
+    random_reaction = random.choice(crowd_reactions)
+    random_space = random.randint(0, len(crowd) - 50)
+
+    for element in crowd:
+        if element != " ":
+            print((random_space * " ") + random_reaction)
+            break
+    print(crowd, end="")
